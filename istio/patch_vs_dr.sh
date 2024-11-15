3552a2b266ba93c11dbb8b965dc9efa45c64cdf4
Sangharsh Boudhh <sboudhh@gmail.com>
2024-11-15 11:26:36 +0000

    Update VS and DR via kubectl patch

---
+++ istio/patch_vs_dr.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+kubectl patch virtualservice hello-1 --type json -p '
+[
+  {
+    "op": "add",
+    "path": "/spec/http/0",
+    "value": {
+      "match": [
+        {
+          "headers": {
+            "baggage": {
+              "regex": ".*hello-1:v2.*"
+            }
+          }
+        }
+      ],
+      "route": [
+        {
+          "destination": {
+            "host": "hello-1",
+            "subset": "v2"
+          }
+        }
+      ]
+    }
+  }
+]'
+
+kubectl patch destinationrule hello-1 --type json -p '
+[
+  {
+    "op": "add",
+    "path": "/spec/subsets/0",
+    "value": {
+      "name": "v2",
+      "labels": {
+        "version": "v2"
+      }
+    }
+  }
+]'
+
+kubectl patch virtualservice hello-2 --type json -p '
+[
+  {
+    "op": "add",
+    "path": "/spec/http/0",
+    "value": {
+      "match": [
+        {
+          "headers": {
+            "baggage": {
+              "regex": ".*hello-2:v2.*"
+            }
+          }
+        }
+      ],
+      "route": [
+        {
+          "destination": {
+            "host": "hello-2",
+            "subset": "v2"
+          }
+        }
+      ]
+    }
+  }
+]'
+
+kubectl patch destinationrule hello-2 --type json -p '
+[
+  {
+    "op": "add",
+    "path": "/spec/subsets/0",
+    "value": {
+      "name": "v2",
+      "labels": {
+        "version": "v2"
+      }
+    }
+  }
+]'
