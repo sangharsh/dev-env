7ed8cccb76a724b2eb28982e248a307072d37a2e
Sangharsh Boudhh <sboudhh@gmail.com>
2024-11-18 11:14:35 +0000

    Simplify logs

--- admctl/internal/clients/istio-client.go
+++ admctl/internal/clients/istio-client.go
@@ -34,19 +34,17 @@ func NewIstioClient() (*IstioClient, error) {
 }
 
 func (client *IstioClient) GetDestinationRules(namespace string) error {
-
 	destinationRules, err := client.client.NetworkingV1().DestinationRules(namespace).List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to list destination rules: %v", err)
 	}
 
 	for _, dr := range destinationRules.Items {
-		log.Printf("DestinationRule: %s in namespace %s", dr.Name, dr.Namespace)
+		log.Printf("DestinationRule: %v", dr)
 	}
 	return nil
 }
 func (client *IstioClient) GetVirtualServices(namespace string) error {
-
 	// List VirtualServices in the specified namespace
 	// Use "" for namespace to list across all namespaces
 	virtualServices, err := client.client.NetworkingV1beta1().VirtualServices(namespace).List(context.TODO(), metav1.ListOptions{})
@@ -57,19 +55,7 @@ func (client *IstioClient) GetVirtualServices(namespace string) error {
 	// Process the virtual services
 	for _, vs := range virtualServices.Items {
 		// Access virtual service properties
-		log.Printf("VirtualService: %s in namespace %s\n", vs.Name, vs.Namespace)
-		// Access hosts
-		for _, host := range vs.Spec.Hosts {
-			log.Printf("  Host: %s\n", host)
-		}
-		// Access gateways
-		for _, gateway := range vs.Spec.Gateways {
-			log.Printf("  Gateway: %s\n", gateway)
-		}
-		// Access HTTP routes
-		for _, http := range vs.Spec.Http {
-			log.Printf("  HTTP Route: %+v\n", http)
-		}
+		log.Printf("VirtualService: %v", vs)
 	}
 
 	return nil
